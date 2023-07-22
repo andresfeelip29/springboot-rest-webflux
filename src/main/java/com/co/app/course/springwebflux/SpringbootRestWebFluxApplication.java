35a1fccfb5093642e1e6cf96f372b6337f9f860c
andresfeelip29 <andreefeelip29@gmail.com>
2023-07-22 18:07:35 -0500

    add chenage from apllications properties, and add eureka discovery client

--- src/main/java/com/co/app/course/springwebflux/SpringbootRestWebFluxApplication.java
+++ src/main/java/com/co/app/course/springwebflux/SpringbootRestWebFluxApplication.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
 import java.util.Date;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringbootRestWebFluxApplication implements CommandLineRunner {
 
